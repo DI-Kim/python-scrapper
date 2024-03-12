3fc01459e7ca64e14c6ae41edce2b11d454963e1
bigperson <daein7076@gmail.com>
2024-03-12 14:19:16 +0900

    second(2.4 ~ 2.8), quiz

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
-print("hello world")
-print('hello world')
+def say_hello(name, age):
+  print(f"hello {name}, you are a {age} years old")
+  
+def say_bye():
+  print("bye bye")
 
-my_name = 'bigPerson'
-
-
-print(f'type of {my_name}: {type(my_name)}')
+x = True
+while x:
+  username = input("enter your name(input 'exit' is go out):")
+  age = input("enter your age")
+  if username == 'exit':
+    x = False
+  else:
+    say_hello(username, age)
