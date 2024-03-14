80dc1380cda55041e3df7a387a5f0ca66b6a7309
bigperson <daein7076@gmail.com>
2024-03-14 14:25:37 +0900

    3.3 ~ 3.7 python casino

--- example.py
+++ example.py
@@ -1,16 +1,19 @@
-def get_yearly_revenue(monthly_revenue):
-#monthly_revenue (월간 매출)를 인수로 받고, revenue for a year (연간 매출)를 리턴.
-  return monthly_revenue * 12
+from random import randint
 
-def get_yearly_expenses(monthly_expenses):
-#monthly_expenses (월간 비용)를 인수로 받고, expenses for a year (연간 비용)를 리턴.
-  return monthly_expenses * 12
-def get_tax_amount (profit):
-#profit (이익) 를 인수로 받고, tax_amount (세금 금액) 를 리턴.
-  if profit > 100000:
-    return profit * 0.25
-  else:
-    return profit * 0.15
-def apply_tax_credits (tax_amount, tax_credits):
-#tax_amount (세금 금액), tax_credits (세액 공제율)를 인수로 받고, amount to discount (할인할 금액)를 리턴.
-  return tax_amount * tax_credits
+print("welcom to Python Casino!")
+
+pc_choice = randint(1, 100)
+
+playing = True
+
+while playing:
+  user_choice = int(input('choose your number (1 - 100):'))
+
+  if user_choice == pc_choice:
+    playing = False
+    print('You won!')
+  elif user_choice > pc_choice:
+    print('Lower!')
+  elif user_choice < pc_choice:
+    print('Higher!')
+    
